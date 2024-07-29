f0637ccd7a2b84ffdb210a523bad7330c38c94c8
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-07-29 23:38:49 +0530

    Fixed card wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,10 @@ import CardWrapper from "./CardWrapper";
 
 function App() {
   const cardContainer = cardData.map((card) => (
-    <Cardcomp
-      key={card.id}
-      card={card}
-    />
+    <Cardcomp key={card.id} card={card} />
   ));
 
-  return (
-    <CardWrapper className="container d-flex flex-wrap justify-content-center mt-5">
-      {cardContainer}
-    </CardWrapper>
-  );
+  return <CardWrapper>{cardContainer}</CardWrapper>;
 }
 
 export default App;
